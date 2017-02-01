eb161e774ab7019137bbade4fc0b69377f4d15ea
Robert Schroeder <robschroeder333@users.noreply.github.com>
2017-02-01 11:45:26 -0500

    updated

--- public/js/custom-script.js
+++ public/js/custom-script.js
@@ -1,14 +1,35 @@
 $(function(){
 
-$('button[data-type="hotel"]').click(function(event) {
-        console.log('clicky!!!')
-        let choice = $('#hotel-choices')[0].selectedIndex;
-        let choiceText = $('#hotel-choices option')[choice].textContent
-        console.log (typeof choiceText)
-        let listTarget = $('.list-group[data-type = "hotelList"]')
-        let elementToAdd =  `<div class="itinerary-item"><span class="title">${choiceText}</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
-        listTarget.append(elementToAdd)
+  //Handles ADD buttons
+  ['hotel', 'restaurant', 'activity'].forEach((ele) => {
+    $(`button[data-type="${ele}"]`).click(function(event) {
+      let choice = $(`#${ele}-choices`)[0].selectedIndex;
+      let choiceText = $(`#${ele}-choices option`)[choice].textContent;
+      let listTarget = $(`.list-group[data-type = "${ele}List"]`);
+      //adjust logic for including multiple activities per day
+      if (!listTarget.children().length){
+        let elementToAdd = `<div class="itinerary-item"><span class="title">${choiceText}</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`;
+        listTarget.append(elementToAdd);
+      } else {
+        alert(`You have already booked a/an ${ele} for this day. (delete your choice of hotel to add a new one)`);
+      }
+    });
+  });
 
-      })
+  //remove items from itinerary
+  $( "#itinerary" ).on( "click", ".remove", function( event ) {
+    $(this).parent().remove();
+  });
 
-})
+  //add new days to itinerary
+  $('#day-add').on( 'click', function(){
+    let dayNumber = $('.day-buttons').children().length;
+    $(`<button class="btn btn-circle day-btn">${dayNumber}</button>`).insertBefore($(this));
+  });
+
+  //change current day
+  $('body').on('click', '.day-btn',function(){
+    $('.current-day').removeClass('current-day');
+    $(this).addClass('current-day')
+  });
+});
